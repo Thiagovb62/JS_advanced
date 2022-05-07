387cd8845839861489b5a959bf0924da6ee56928
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-07 07:45:07 -0300

    tipos de funçoes..

--- JS_Advanced_Functions/aula-1/index.js
+++ JS_Advanced_Functions/aula-1/index.js
@@ -1,29 +1,29 @@
 // Declaraçao de funçao (Function hoisting)
 falaOi();
-function falaOi(){
-    console.log('Oie')
+function falaOi() {
+  console.log("Oie");
 }
-// Obejetos de primeira classe 
+// Obejetos de primeira classe
 // Function expression
-const souUmDado = function(){
-    console.log('Sou um dado')
-}
+const souUmDado = function () {
+  console.log("Sou um dado");
+};
 souUmDado();
 // variaveis recebendo funçoes
-function executaFunçao(funcao){
-    console.log('Vou executar sua funçao abaixo')
-    funcao();
+function executaFunçao(funcao) {
+  console.log("Vou executar sua funçao abaixo");
+  funcao();
 }
 executaFunçao(souUmDado);
-// Arrow function 
+// Arrow function
 const functionArrow = () => {
-    console.log('Arrow function')
-}
+  console.log("Arrow function");
+};
 functionArrow();
 //Dentro de um objeto
-const obj = { 
-    falar() {
-        console.log('Dentro de um objeto')
-    }
-}
+const obj = {
+  falar() {
+    console.log("Dentro de um objeto");
+  },
+};
 obj.falar();
