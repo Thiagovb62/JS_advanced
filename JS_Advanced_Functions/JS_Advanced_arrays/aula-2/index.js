78a615a5ace5289e1c4871b85e019a27d27ff855
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-12 14:48:21 -0300

    Metodo splic().

---
+++ JS_Advanced_Functions/JS_Advanced_arrays/aula-2/index.js
@@ -0,0 +1,20 @@
+//              -5   -4   -3   -2    -1
+//               0    1    2    3    4
+const nomes  = ['a', 'b', 'c', 'd', 'e'];
+
+// nomes.splice(indice ,delet ,elem1,elem2,... );
+const removidos = nomes.splice(3,2,'luiz','junio');
+const inserindo = nomes.splice(2,0,'J');
+
+//pop
+//const removidos = nomes.splice(-1,1);
+
+//shift
+//const removidos = nomes.splice(0,1,'l');
+
+//shift
+//const removidos = nomes.splice(0,0,'lu');
+
+//push
+//const removidos = nomes.splice(nomes.length,0,1);
+console.log(nomes, removidos);
