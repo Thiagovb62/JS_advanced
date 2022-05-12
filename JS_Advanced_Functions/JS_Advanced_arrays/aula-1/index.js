0ad95dffe8bde48fd44c489ceb93ddf7ded56189
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-12 14:17:58 -0300

    add nova pasta para arrays e revisando sintaxe de array.

---
+++ JS_Advanced_Functions/JS_Advanced_arrays/aula-1/index.js
@@ -0,0 +1,19 @@
+// Valor por referencia
+const nomes = ["a", "b", "c", "d", "e", "f"];
+//const nomes = new Array('a', 'b', 'c', 'd', 'e',)
+const novo = [...nomes];
+console.log(novo);
+//const removido = nomes.pop() remove e mostra  o nome f
+//const desloca = nomes.shift() todo mundo volta um indice e o antigo elemento do indice 0 some
+//nomes.push('') add algo no array
+nomes.unshift("Thiago");
+nomes.slice(3, -1); // divide o array
+nomes[2] = "k";
+delete nomes[2];
+console.log(nomes);
+
+// tranformando string em array
+const name = "Luiz Otavio Silva";
+const names = name.split(" ");
+// name.join() caso fosse um array para string
+console.log(names);
